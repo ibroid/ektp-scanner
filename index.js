d2900944fe3900cb6bf6e07741961ef2b6cc7705
ibroid <maulana858@gmail.com>
2022-07-06 22:28:59 +0700

    fiksasi upload. tinggal recursive

--- index.js
+++ index.js
@@ -2,41 +2,70 @@ import csv from "csvtojson";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from 'url';
-
+import * as dotenv from 'dotenv'
+import fetch, { fileFromSync, FormData } from "node-fetch"
+import csvToJson from "convert-csv-to-json";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const converter = csv({
-    delimiter: "\t",
-    ignoreEmpty: true,
+	delimiter: "\t",
+	ignoreEmpty: true,
 })
-console.log(__dirname)
-const hasilPath = path.join(__dirname, '/bckp/');
+dotenv.config()
+
+const hasilPath = path.join(__dirname, process.env.RESULT_DIR);
+
 function readFiles(dirname) {
-    fs.readdir(dirname, function (err, files) {
-        if (err) {
-            console.log(err)
-            return false;
-        }
+	fs.readdir(dirname, async function (err, files) {
+		if (err) {
+			console.log(err)
+			return false;
+		}
 
-        if (files.length > 0) {
+		if (files.length > 0) {
 
-            files.forEach(file => {
+			const body = new FormData();
+			files.forEach((file) => {
 
-                if (file.includes('.csv')) {
+				if (file.includes('.csv')) {
 
-                    fs.readFile(hasilPath + file, "utf16le", (err, data) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                        converter.fromString(data).then(res => console.log(res));
-                    })
-                }
-            });
-        }
+					const result = csvToJson.fieldDelimiter('\t').utf16leEncoding().getJsonFromCsv(hasilPath + file)
+
+					body.append('nik', result[0].NIK);
+					body.append('nama', result[0].Nama);
+					body.append('jenis_kelamin', result[0]['Jenis Kelamin']);
+					body.append('alamat', `${result[0].Alamat} RT/RW ${result[0]['RT/RW']}`);
+					body.append('kelurahan', result[0]['Kel/Desa']);
+					body.append('kecamatan', result[0].Kecamatan);
+					body.append('kota', result[0].Kota);
+					body.append('provinsi', result[0].Provinsi);
+					body.append('Pekerjaan', result[0].Pekerjaan);
+
+				}
+
+				if (file.includes('.jpg')) {
+					if (file.includes('_Photo')) {
+						body.set('foto', fileFromSync(hasilPath + file))
+					} else {
+
+						body.set('ktp', fileFromSync(hasilPath + file))
+					}
+				}
+
+			});
+
+			const requestUpload = await fetch('http://visitor.pa-jakartautara.go.id/debug', {
+				method: "POST",
+				body: body
+			}).then(res => res.json())
+
+			console.log(requestUpload)
+		}
 
 
-    });
+	});
 }
 
 readFiles(hasilPath)
